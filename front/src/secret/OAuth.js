99dbf0f895630ca797d302f8a222ac520fe883bf
seojin-kim12 <80455266+seojin-kim12@users.noreply.github.com>
2025-01-17 18:46:01 +0900

    Delete front/src/secret/OAuth.js

--- front/src/secret/OAuth.js
+++
@@ -1,4 +0,0 @@
-const REST_API_KEY  = "a5bf039b62d2837be3d07e455d8bda52";
-const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
